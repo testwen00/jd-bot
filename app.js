86eb50525cb2f60a55d4900eda9055808bc2a201
chesterchenn <chesterchenn@gmail.com>
2021-03-18 13:16:40 +0800

    refactor: 更新依赖及代码

--- app.js
+++ app.js
@@ -1,44 +1,55 @@
-// version v0.0.2
-// create by ruicky
-// detail url: https://github.com/ruicky/jd_sign_bot
+/**
+ * 下载JD_DailyBonus.js，然后替换变量，进行执行
+ */
 
 const exec = require('child_process').execSync;
 const fs = require('fs');
-const rp = require('request-promise');
-const download = require('download');
+const fetch = require('node-fetch');
+const FormData = require('form-data');
+
+const form = new FormData();
+const JSFile = 'JD_DailyBonus.js';
 
 // 公共变量
 const KEY = process.env.JD_COOKIE;
 const serverJ = process.env.PUSH_KEY;
 const DualKey = process.env.JD_COOKIE_2;
 
-async function downFile() {
-  // const url = 'https://cdn.jsdelivr.net/gh/NobyDa/Script@master/JD-DailyBonus/JD_DailyBonus.js';
-  const url = 'https://raw.githubusercontent.com/NobyDa/Script/master/JD-DailyBonus/JD_DailyBonus.js';
-  await download(url, './');
+async function downloadJS() {
+  console.log('开始下载代码...')
+  const url = `https://cdn.jsdelivr.net/gh/NobyDa/Script@master/JD-DailyBonus/${JSFile}`;
+  // const url = `https://raw.githubusercontent.com/NobyDa/Script/master/JD-DailyBonus/${JSFile}`;
+  const res = await fetch(url);
+  const data = await res.text();
+  fs.writeFileSync(JSFile, data, err => {
+    if (err) throw err;
+    console.log('下载代码完毕!');
+  })
 }
 
-async function changeFile() {
-  let content = await fs.readFileSync('./JD_DailyBonus.js', 'utf8');
+function changeFile() {
+  console.log('开始替换变量...')
+  let content = fs.readFileSync(JSFile, 'utf8');
   content = content.replace(/var Key = ''/, `var Key = '${KEY}'`);
   if (DualKey) {
     content = content.replace(/var DualKey = ''/, `var DualKey = '${DualKey}'`);
   }
-  await fs.writeFileSync('./JD_DailyBonus.js', content, 'utf8');
+  fs.writeFileSync(JSFile, content, 'utf8', err => {
+    if (err) throw err;
+    console.log('替换变量完毕!');
+  });
 }
 
-async function sendNotify(text, desp) {
+async function sendNotify(title, desp) {
+  const url = `https://sctapi.ftqq.com/${serverJ}.send`;
+  form.append('title', title);
+  form.append('desp', desp);
   const options = {
-    uri: `https://sc.ftqq.com/${serverJ}.send`,
-    form: { text, desp },
-    json: true,
     method: 'POST',
+    body: form,
   };
-  await rp.post(options).then((res) => {
-    console.log(res);
-  }).catch((err) => {
-    console.log(err);
-  });
+  const res = await fetch(url, options)
+  console.log(res);
 }
 
 async function start() {
@@ -47,14 +58,15 @@ async function start() {
     return;
   }
   // 下载最新代码
-  await downFile();
-  console.log('下载代码完毕');
+  await downloadJS();
+
   // 替换变量
   await changeFile();
-  console.log('替换变量完毕');
+
   // 执行
-  await exec('node JD_DailyBonus.js >> result.txt');
-  console.log('执行完毕');
+  console.log('开始执行...')
+  await exec(`node ${JSFile} >> result.txt`);
+  console.log('执行完毕!');
 
   if (serverJ) {
     const path = './result.txt';
